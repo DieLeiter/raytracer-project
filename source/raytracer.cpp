97ae36e04af8be7996991283eae9bb069e2feed3
TonyZoeppig <tonyjan98@gmail.com>
2020-08-12 18:55:26 +0200

    started trace function in raytracer

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -10,6 +10,8 @@
 
 #include "sdfparser.hpp"
 #include "scenegraph.hpp"
+#include "ray.hpp"
+#include "shape.hpp"
 
 //now single threaded again
 int main(int argc, char* argv[])
@@ -34,14 +36,29 @@ int main(int argc, char* argv[])
 
 
 
- /* Window window{{image_width, image_height}};
+    Window window{{image_width, image_height}};
 
-  while (!window.should_close()) {
-    if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-      window.close();
+    while (!window.should_close()) {
+        if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+         window.close();
+        }
+        window.show(renderer.color_buffer());
     }
-    window.show(renderer.color_buffer());
-  }*/
 
   return 0;
+}
+
+void trace(Scenegraph& scene, Renderer &renderer, unsigned image_width, unsigned image_height) {
+
+    float distance = (image_width / 2) / std::tan(scene.camera->fov_x / 2);
+
+    for (unsigned i = image_height/2; i > -image_height/2; --i) {
+        for (unsigned e = -image_width/2; e < image_width/2; ++e) {
+            Ray ray{ glm::vec3(0, 0, 0), glm::vec3(e, i, distance) };
+
+            for each(std::shared_ptr<Shape> shape in scene.objects) {
+                
+            }
+        }
+    }
 }
