19abe559c74bdc553466b5040a45569498f6d8ce
Lina Klass <linaklass@MacBook-Air.local>
2020-08-16 12:30:49 +0200

    changed distance computation: radian instead degree

--- framework/tracer.cpp
+++ framework/tracer.cpp
@@ -1,17 +1,20 @@
 #include "tracer.hpp"
 #include <algorithm>
+#include <math.h>
 
 void Tracer::trace(Scenegraph& scene, Renderer& renderer, unsigned image_width, unsigned image_height) const
 {
     std::cout << "Started raytracing" << std::endl;
 
-    float distance = (image_width / 2) / std::tan(scene.camera->fov_x / 2);
+    //radians = degrees * pi / 180 ;
+    float camera_angle_rad = scene.camera->fov_x * M_PI / 180.0f;
+    float distance = -((image_width / 2) / std::tan(camera_angle_rad / 2));
     std::cout << "Computed Distance: " << distance << std::endl;
 
     for (int y = 0; y < (int)image_height; ++y) {
 
         for (int x = 0; x < (int)image_width; ++x) {
-
+            //TODO: Ray Erzeugung in eigene Kamera Klasse auslagern
             float ray_x = (int)image_width / 2 - ((int)image_width - x);
             float ray_y = (int)image_height / 2 - ((int)image_height - y);
             Ray ray{ glm::vec3(0, 0, 0), glm::vec3(ray_x, ray_y, distance) };
