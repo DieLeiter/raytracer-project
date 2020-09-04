537f0e3d30837b318ab5e2a7d398345e0de6b9f8
TonyZoeppig <tonyjan98@gmail.com>
2020-09-04 19:22:09 +0200

    Continued Animation

--- framework/sdfwriter.cpp
+++ framework/sdfwriter.cpp
@@ -10,36 +10,59 @@ void SdfWriter::write(float progress_percentage, std::string const& filename) co
     myfile.open(filename, std::ios::out);
 
     //write materials
-    myfile << "#materials" << std::endl 
+    myfile << "#materials" << std::endl
         << "define material red 0.7 0 0 0.1 0 0 0.1 0 0 100" << std::endl
-        << "define material blue 0 0 0.7 0 0 0.1 0 0 0.1 10" << std::endl
-        << "define material white 1 1 1 0.1 0.1 0.1 0.1 0.1 0.1 50" << std::endl;
-
-    //write geometry
-    myfile << "#geometry" << std::endl
-        << "define shape box rbottom -100 -50 50 100 50 -50 red" << std::endl
-        << "define shape box floor -200 -105 -200 200 -85 -800 white" << std::endl
-        << "define shape sphere bsphere 0 0 0 30 white" << std::endl;
+        << "define material blue 0 0 0.7 0 0 0.1 0 0 0.1 100" << std::endl
+        << "define material green 0 0.7 0 0 0.1 0 0 0.1 0 100" << std::endl
+        << "define material yellow 0.7 0.7 0 0.1 0.1 0 0.1 0.1 0 100" << std::endl
+        << "define material dark_white 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 50" << std::endl
+        << "define material white 0.7 0.7 0.7 0.2 0.2 0.2 0.2 0.2 0.2 810" << std::endl;
 
     //camera
     myfile << "#camera" << std::endl << "define camera eye 120.0 0 0 0 0 0 -1 0 1 0" << std::endl;
 
-    //scene xform
-    float scale_endvalue = 1.5f;
-    float rotation_endvalue = 45.0f;
+    //write geometry
+    myfile << "#geometry" << std::endl
+        << "define shape box floor -800 -200 800 800 -185 -800 dark_white" << std::endl
+        << "define shape box red_cube -25 -185 25 25 -135 -25 red" << std::endl
+        << "define shape box green_cube -25 -135 25 25 -85 -25 green" << std::endl
+        << "define shape box blue_cube -25 -85 25 25 -35 -25 blue" << std::endl
+        << "define shape box yellow_cube -25 -35 25 25 15 -25 yellow" << std::endl
+        << "define shape sphere white_sphere 50 -155 -100 30 white" << std::endl;
 
+    //camera xform
+    float cam_rotation_endvalue = 90.0f;
+
+    myfile << "#camera xform" << std::endl
+        << "transform eye translate 0 0 300" << std::endl
+        << "transform eye rotate " << std::to_string(cam_rotation_endvalue * progress_percentage) << " 0 1 0" << std::endl;
+
+    //scene xform
+    float red_rotation = 90.0f;
+    float green_rotation = 180.0f;
+    float blue_rotation = 270.0f;
+    float yellow_rotation = 360.0f;
+    float sphere_translation = 200.0f;
+    float sphere_rotation = 360.0f;
+
+    if (progress_percentage <= 0.5f) {
+        myfile << "#scene xform" << std::endl
+            << "transform white_sphere translate 100 " << std::to_string(sphere_translation * progress_percentage) << " 0" << std::endl;
+    }
+    else {
+        myfile << "#scene xform" << std::endl
+            << "transform white_sphere translate 100 " << std::to_string(sphere_translation * (1.0f - progress_percentage)) << " 0" << std::endl;
+    }
     myfile << "#scene xform" << std::endl
-        << "transform rbottom rotate " << std::to_string(rotation_endvalue * progress_percentage) << " 0 1 0" << std::endl
-        << "transform rbottom translate 0 100 -500" << std::endl
-        << "transform bsphere scale " << std::to_string(scale_endvalue * progress_percentage) << " " << std::to_string(scale_endvalue * progress_percentage) << " " << std::to_string(scale_endvalue * progress_percentage) << std::endl
-        << "transform bsphere translate 0 -30 -100" << std::endl;
+        << "transform red_cube rotate " << std::to_string(red_rotation * progress_percentage) << " 0 1 0" << std::endl
+        << "transform green_cube rotate " << std::to_string(green_rotation * progress_percentage) << " 0 1 0" << std::endl
+        << "transform blue_cube rotate " << std::to_string(blue_rotation * progress_percentage) << " 0 1 0" << std::endl
+        << "transform yellow_cube rotate " << std::to_string(yellow_rotation * progress_percentage) << " 0 1 0" << std::endl;
 
     //lights: name, position, color, intensity
     myfile << "#lights" << std::endl 
-        << "define light sun 0 0 0 0.7 0.7 0.7 5" << std::endl
-        //<< "define light sun2 0 0 0 0.75 0.75 0.75 10" << std::endl
-        //<< "define light spot1 0 0 0 0.8 0.8 0.8 10" << std::endl
-        << "ambient 0.5 0.5 0.5" << std::endl;
+        << "define light left_light -200 500 700 0.5 0.5 0.5 3" << std::endl
+        << "ambient 0.1 0.1 0.1" << std::endl;
 
     //render
     myfile << "# ... and go" << std::endl << "render eye image.ppm 800 600" << std::endl;
